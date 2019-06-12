a2dfbdb11628cb1eca6bcfacbafa061caf129746
Fabricio-master <fabrimza7@gmail.com>
2019-06-12 01:08:00 -0300

    Arreglar prueba

--- app/src/main/java/com/example/changosconsumidor/database/ProductDBHelper.java
+++ app/src/main/java/com/example/changosconsumidor/database/ProductDBHelper.java
@@ -57,33 +57,33 @@ public class ProductDBHelper {
         }
     }//createProduct();
 
-     public static void updateProduct(Product product, Context context){
-         int id = product.getID();
-         String mark = product.getMark();
-         String name = product.getName();
-         float contentQuantity = product.getContentQuantity();
-         String contentUnit = product.getContentUnit();
-         int categoryID = product.getCategory().getId();
+    public static void updateProduct(Product product, Context context){
+        int id = product.getID();
+        String mark = product.getMark();
+        String name = product.getName();
+        float contentQuantity = product.getContentQuantity();
+        String contentUnit = product.getContentUnit();
+        int categoryID = product.getCategory().getId();
 
-         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(context);
-         SQLiteDatabase db = admin.getWritableDatabase();
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(context);
+        SQLiteDatabase db = admin.getWritableDatabase();
 
-         //SQLiteStatement stmt = new
-         try{
-             ContentValues record = new ContentValues();
-             record.put("id", id);
-             record.put("mark",mark);
-             record.put("name",name);
-             record.put("contentQuantity", contentQuantity);
-             record.put("contentUnit",contentUnit);
-             record.put("category_id", categoryID);
+        //SQLiteStatement stmt = new
+        try{
+            ContentValues record = new ContentValues();
+            record.put("id", id);
+            record.put("mark",mark);
+            record.put("name",name);
+            record.put("contentQuantity", contentQuantity);
+            record.put("contentUnit",contentUnit);
+            record.put("category_id", categoryID);
 
-             db.insert(DB_TABLE,null,record);
-             db.close();
+            db.insert(DB_TABLE,null,record);
+            db.close();
 
-             Toast.makeText(context, "Producto agregado.", Toast.LENGTH_SHORT).show();
-         }catch(Exception e){
-             Toast.makeText(context, "Error al guardar: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-         }
-     }//updateProduct()
- }
+            Toast.makeText(context, "Producto agregado.", Toast.LENGTH_SHORT).show();
+        }catch(Exception e){
+            Toast.makeText(context, "Error al guardar: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }//updateProduct()
+}
