c396c81689e0dd93692e16d75fc8ede4dbbb6951
Juan M. Panasiti <juanpanasiti@gmail.com>
2019-06-15 22:57:28 -0300

    Agregados los archivos para DBHelper de BuyList e Item que extienden de AdminSQLiteOpenHelper

---
+++ app/src/main/java/com/example/changosconsumidor/database/BuyListDBHelper.java
@@ -0,0 +1,9 @@
+package com.example.changosconsumidor.database;
+
+import android.content.Context;
+
+public class BuyListDBHelper extends AdminSQLiteOpenHelper {
+    public BuyListDBHelper(Context context) {
+        super(context);
+    }
+}
---
+++ app/src/main/java/com/example/changosconsumidor/database/ItemDBHelper.java
@@ -0,0 +1,9 @@
+package com.example.changosconsumidor.database;
+
+import android.content.Context;
+
+public class ItemDBHelper extends AdminSQLiteOpenHelper {
+    public ItemDBHelper(Context context) {
+        super(context);
+    }
+}
