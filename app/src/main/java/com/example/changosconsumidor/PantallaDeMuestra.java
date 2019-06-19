8cc0d0787724c9dfbcc6dca13961f1ee2d8152d6
JulioAravena <relola15@gmail.com>
2019-06-18 22:15:43 -0300

    Creacion de metodos de registro

---
+++ app/src/main/java/com/example/changosconsumidor/PantallaDeMuestra.java
@@ -0,0 +1,12 @@
+package com.example.changosconsumidor;
+
+import android.os.Bundle;
+
+public class PantallaDeMuestra {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mostrar_info);
+
+}
---
+++ app/src/main/java/com/example/changosconsumidor/Registro.java
@@ -0,0 +1,47 @@
+package com.example.changosconsumidor;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+
+        final EditText nombreUsuario = (EditText) findViewById(R.id.etNuevoNombre);
+        final EditText contraseña = (EditText) findViewById(R.id.etNuevaContra);
+        final EditText email = (EditText) findViewById(R.id.etNuevoEmail);
+        Button btnRegistro = (Button) findViewById(R.id.botonNuevoRegistro);
+
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferencias = getSharedPreferences("Mis Preferencias", MODE_PRIVATE);
+                String nuevoUsuario = nombreUsuario.getText().toString();
+                String nuevoContra = contraseña.getText().toString();
+                String nuevoEmail = email.getText().toString();
+
+                SharedPreferences.Editor editor = preferencias.edit();
+
+                editor.putString(nuevoUsuario + nuevoContra +"Datos"+ nuevoUsuario + "/n" + nuevoEmail  );
+                editor.commit();
+
+
+
+            }
+
+
+
+        });
+
+
+}
+}
+
