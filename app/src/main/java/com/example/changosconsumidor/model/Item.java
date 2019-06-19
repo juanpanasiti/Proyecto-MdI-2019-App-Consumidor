796e5ab0e65ddf27947b9ebc223ca4440e25389d
Fabricio-master <fabrimza7@gmail.com>
2019-06-19 01:25:24 -0300

    metodso agregar modificar borrar y buscar

--- app/src/main/java/com/example/changosconsumidor/model/Item.java
+++ app/src/main/java/com/example/changosconsumidor/model/Item.java
@@ -1,5 +1,11 @@
 package com.example.changosconsumidor.model;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.example.changosconsumidor.database.ItemDBHelper;
+import com.example.changosconsumidor.database.ProductDBHelper;
+
 public class Item {
     private Product product;
     private BuyList buyList;
@@ -60,6 +66,68 @@ public class Item {
         this.quantity=quantity;
     }
 
+//metodos crear modificar borrar y buscar
 
+    public boolean crear(Context context){
+        ItemDBHelper ItemDBH = new ItemDBHelper(context);
+        try{
+            ItemDBH.createItem(this, context);
+            Toast.makeText(context, "Item creado correctamente", Toast.LENGTH_SHORT).show();
+            return true;
+        } catch (Exception e){
+            Toast.makeText(context, "Error al crear Item " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    //update
+    public boolean modificar(Context context){
+        ItemDBHelper ItemDBH = new ItemDBHelper(context);
+        try{
+            ItemDBH.updateItem(this, context);
+            Toast.makeText(context,"Item modificado ",Toast.LENGTH_SHORT).show();
+            return true;
+        } catch (Exception e){
+            Toast.makeText(context, "Error al modificar"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+    //find id
+    public boolean Buscar(Context context){
+        ItemDBHelper ItemDBH = new ItemDBHelper(context);
+        try{
+            ItemDBH.findByID(id,context);//revisa!!!!!!!!
+            Toast.makeText(context,"Item encontrado",Toast.LENGTH_SHORT).show();
+            return true;
+        } catch (Exception e){
+            Toast.makeText(context, "Item no encontrado"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+    //deleteproduct
+    public boolean BorrarReg(Context context){
+        ItemDBHelper ItemDBH = new ItemDBHelper(context);
+        try{
+            ItemDBH.deleteItem(this,context);
+            Toast.makeText(context,"Item borrado",Toast.LENGTH_SHORT).show();
+            return true;
+        } catch (Exception e){
+            Toast.makeText(context, "Item no borrado"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+    //getall
+    public boolean traertodo(Context context){
+        ItemDBHelper ItemDBH = new ItemDBHelper(context);
+        try{
+            ItemDBH.getAll(context);
+            Toast.makeText(context,"Items cargados",Toast.LENGTH_SHORT).show();
+            return true;
+        } catch (Exception e){
+            Toast.makeText(context, "Items no cargados"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
 
 }
