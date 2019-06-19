3cb09955ef20d2f3254685b9814a7ffdbb9fbe2c
Fabricio-master <fabrimza7@gmail.com>
2019-06-19 00:17:15 -0300

    Agego metodos get y set itme

--- app/src/main/java/com/example/changosconsumidor/model/Item.java
+++ app/src/main/java/com/example/changosconsumidor/model/Item.java
@@ -5,5 +5,61 @@ public class Item {
     private BuyList buyList;
     private float quantity;
     private float unitPrice;
-    private boolean intoCart;
+    private boolean intoChart;
+    private int id;
+
+    //metodos get y set
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id){
+        this.id=id;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public void setIntoChart(boolean intoChart) {
+        this.intoChart = intoChart;
+    }
+
+    public boolean isIntoChart() {
+        return intoChart;
+    }
+
+
+    public float getUnitPrice(){
+        return unitPrice;
+    }
+
+    public void setProduct(Product product){
+        this.product=product;
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+
+    public BuyList getBuyList() {
+        return buyList;
+    }
+
+
+    public void setBuyList(BuyList buyList){
+        this.buyList=buyList;
+    }
+
+    public float getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(float quantity){
+        this.quantity=quantity;
+    }
+
+
+
 }
