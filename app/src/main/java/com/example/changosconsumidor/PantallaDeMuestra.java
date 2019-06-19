664c587420e6c290c301b942630af6421cc74bdc
JulioAravena <relola15@gmail.com>
2019-06-18 22:41:04 -0300

    Se completo la clase PantallaDeMuestra

--- app/src/main/java/com/example/changosconsumidor/PantallaDeMuestra.java
+++ app/src/main/java/com/example/changosconsumidor/PantallaDeMuestra.java
@@ -1,12 +1,23 @@
 package com.example.changosconsumidor;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class PantallaDeMuestra {
+import org.w3c.dom.Text;
+
+public class PantallaDeMuestra extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mostrar_info);
+
+        SharedPreferences preferencias = getSharedPreferences("Mis Preferencias", MODE_PRIVATE);
+        String mostrar = preferencias.getString("Datos","");
+
+        TextView mostrarInfo = (TextView) findViewById(R.id.txtDatos);
+        mostrarInfo.setText(mostrar);
 
 }
--- app/src/main/java/com/example/changosconsumidor/mainRegistro.java
+++ app/src/main/java/com/example/changosconsumidor/mainRegistro.java
@@ -1,13 +1,16 @@
 package com.example.changosconsumidor;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class mainRegistro {
+public class mainRegistro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
 
 }
 }
