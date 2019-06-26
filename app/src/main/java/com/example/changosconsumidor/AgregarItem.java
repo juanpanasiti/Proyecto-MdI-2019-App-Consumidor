a10cb9514f4cebf523d0e507336ed21254ed5ea1
MatiMartinez <matia.martine2018@gmail.com>
2019-06-26 17:02:40 -0300

    App funcionando! A testear

--- app/src/main/java/com/example/changosconsumidor/AgregarItem.java
+++ app/src/main/java/com/example/changosconsumidor/AgregarItem.java
@@ -4,13 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.example.changosconsumidor.model.Category;
+import com.example.changosconsumidor.model.Item;
+import com.example.changosconsumidor.model.Product;
+
+import java.util.ArrayList;
 
 public class AgregarItem extends AppCompatActivity {
 
     private Spinner spinner_productos, spinner_categorias;
     private EditText campoCantidad, campoPrecio;
+
+    private Product prod = new Product();
+    private ArrayList<Product> arrProductos;
+    private ArrayList<String> arrProductosStr;
+
+    private Category cat = new Category();
+    private ArrayList<Category> arrCategorias;
+    private ArrayList<String> arrCategoriasStr;
+
+    private Item item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,7 @@ public class AgregarItem extends AppCompatActivity {
         Intent intent = null;
         switch(view.getId()) {
             case R.id.btn_de_agregarItem_a_list:
+
                 intent = new Intent();
                 break;
             case R.id.btnAgregarItemFinal:
@@ -36,4 +54,20 @@ public class AgregarItem extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    private void rellenarSpinnerCategorias() {
+        arrCategorias = new ArrayList<>();
+        arrCategorias = cat.traerTodo(AgregarItem.this);
+        arrCategoriasStr = new ArrayList<>();
+        for(int i = 0; i < arrCategorias.size(); i++) {
+            arrCategoriasStr.add(arrCategorias.get(i).getName());
+        }
+        ArrayAdapter<String> categorias = new ArrayAdapter<>(AgregarItem.this, android.R.layout.simple_spinner_item, arrCategoriasStr);
+        spinner_categorias.setAdapter(categorias);
+    }
+
+    public void llenarSpinnerProductos() {
+
+    }
+
 }
